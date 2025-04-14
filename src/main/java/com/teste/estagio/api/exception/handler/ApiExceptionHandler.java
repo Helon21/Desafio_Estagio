3e7dbc743c7535dc3cca88a611da34f7717b86db
Helon21 <helonbentes51@hotmail.com>
2025-04-13 22:13:58 -0300

    feat: add global exception handling with custom error messages

---
+++ src/main/java/com/teste/estagio/api/exception/handler/ApiExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.teste.estagio.api.exception.handler;
+
+import com.teste.estagio.api.exception.CustomerNotFoundException;
+import com.teste.estagio.api.exception.DuplicateFieldException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(DuplicateFieldException.class)
+    public ResponseEntity<ErrorMessage> duplicateFieldException(DuplicateFieldException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.CONFLICT, ex.getMessage()));
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ErrorMessage> customerNotFoundException(CustomerNotFoundException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+}
